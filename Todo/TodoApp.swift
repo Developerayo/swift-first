fec242a395deaf9612a7d1f13ffb019f30f815fc
Shodipo Ayomide <shodipovi@gmail.com>
2023-09-27 02:37:15 +0100

    Initial Commit

---
+++ Todo/TodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoApp.swift
+//  Todo
+//
+//  Created by Shodipo Victory on 27/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct TodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
